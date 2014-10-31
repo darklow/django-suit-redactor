b713d0856765c23420492343a462ea5cb207a145
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2014-10-31 13:49:32 +0100

    Set focus on Redactor when clicking on Redactor's main div

--- suit_redactor/widgets.py
+++ suit_redactor/widgets.py
@@ -5,6 +5,10 @@ from django.utils.safestring import mark_safe
 try: import json
 except ImportError: import django.utils.simplejson as json
 
+FOCUS_SCRIPT = """
+    $('.redactor_box').click(function(e){if(!$(e.srcElement).is('.redactor_toolbar')){$('.redactor_editor').focus();}});
+"""
+
 
 class RedactorWidget(Textarea):
     class Media:
@@ -21,6 +25,6 @@ class RedactorWidget(Textarea):
     def render(self, name, value, attrs=None):
         output = super(RedactorWidget, self).render(name, value, attrs)
         output += mark_safe(
-            '<script type="text/javascript">$("#id_%s").redactor(%s);</script>'
-            % (name, json.dumps(self.editor_options)))
+            '<script type="text/javascript">$("#id_%s").redactor(%s);%s</script>'
+            % (name, json.dumps(self.editor_options), FOCUS_SCRIPT))
         return output
