3af2a27c63fe7490d1d529878f5ef2e3c9e687df
Kaspars Sprogis <darklow@gmail.com>
2016-04-27 15:59:04 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-suit-redactor',
-    version='0.0.3',
+    version='0.0.4',
     description='Imperavi Redactor (WYSIWYG editor) integration app for Django admin. http://imperavi.com/redactor/',
     author='Kaspars Sprogis (darklow)',
     author_email='info@djangosuit.com',
